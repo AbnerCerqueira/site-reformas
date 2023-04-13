ccf11f1ad609ebe126ad47d5673c53a060606b03
AbnerCerqueria <abner.cerqueira@etec.sp.gov.br>
2023-04-13 17:58:11 -0300

    commit inicial

---
+++ sitesan/js.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({reset: true,distance:'100px'});
+
+
+sr.reveal('.frase', {duration:1000,origin: 'left' })
+sr.reveal('.caixa', {duration:1000,origin: 'right'})
+
+
+sr.reveal('.alvenaria', {origin: 'left' })
+sr.reveal('.AlvenariaImg', {origin: 'right' })
+
+
