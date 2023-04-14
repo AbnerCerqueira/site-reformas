f60a48f2833ed8f14431c813592cad7a7a257b27
Matheus <matheus.mariano28@etec.sp.gov.br>
2023-04-14 15:03:56 -0300

    mudança tag As

--- sitesan/js.js
+++ sitesan/js.js
@@ -1,17 +1,17 @@
 window.sr = ScrollReveal({reset: true});
 
 
-sr.reveal('.frase', {duration:1000})
-sr.reveal('.caixa', {duration:1000})
+sr.reveal('.frase', {duration:1300})
+sr.reveal('.caixa', {duration:1300})
 
 
-sr.reveal('.infos', {duration:1000})
+sr.reveal('.infos', {duration:1300})
 
 
-sr.reveal('.alvenaria', {duration:1000})
-sr.reveal('.AlvenariaImg', {duration:1000})
+sr.reveal('.alvenaria', {duration:1300})
+sr.reveal('.AlvenariaImg', {duration:1300})
 
-sr.reveal('.eletrica', {duration:1000})
-sr.reveal('.EletricaImg', {duration:1000})
+sr.reveal('.eletrica', {duration:1300})
+sr.reveal('.EletricaImg', {duration:1300})
 
 
