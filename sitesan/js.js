6eea8273fbe55e4c6481f70aafd668cfebd7dc0f
matheus <matheus.mariano28@etec.sp.gov.br>
2023-04-28 15:35:24 -0300

    responsividade e menu hamburguer

--- sitesan/js.js
+++ sitesan/js.js
@@ -1,7 +1,5 @@
 window.sr = ScrollReveal({reset: true});
 
-
-sr.reveal('.frase', {duration:1300})
 sr.reveal('.caixa', {duration:1300})
 
 
@@ -14,4 +12,18 @@ sr.reveal('.AlvenariaImg', {duration:1300})
 sr.reveal('.eletrica', {duration:1300})
 sr.reveal('.EletricaImg', {duration:1300})
 
+var menu = document.querySelector("#menu")
+var teste = 0
+function openMenu(){
+    if (teste == 0) {      
+        menu.src="img/close.svg"
+        menu.style.width="30px"
+        menu.style.height="30px"
+        teste = 1
+    }else{
+        menu.src="img/menu.svg"
+        teste = 0
+    }
+
+}
 
