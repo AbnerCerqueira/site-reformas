f6c153c8c60566a5fec84ce630d0834fd0c0452b
Matheus <matheus.mariano28@etec.sp.gov.br>
2023-04-29 21:35:43 -0300

    responsividade finalizada

--- sitesan/js.js
+++ sitesan/js.js
@@ -12,7 +12,11 @@ sr.reveal('.AlvenariaImg', {duration:1300})
 sr.reveal('.eletrica', {duration:1300})
 sr.reveal('.EletricaImg', {duration:1300})
 
+
+/*___________________________________________________________________________________________________________*/ 
+
 var menu = document.querySelector("#menu")
+var menuaparece = document.querySelector("#menu-aparece")
 var teste = 0
 function openMenu(){
     if (teste == 0) {      
@@ -20,9 +24,11 @@ function openMenu(){
         menu.style.width="30px"
         menu.style.height="30px"
         teste = 1
+        menuaparece.style.height="200px"
     }else{
         menu.src="img/menu.svg"
         teste = 0
+        menuaparece.style.height="0px"
     }
 
 }
